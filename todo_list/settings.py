f4be36ac4d1b9fce42ac94eb3ea7b6bc4dd40c1a
Omar Shemy <OmarShemy@Omar-Shemys-MacBook-Pro.local>
2024-01-22 19:42:49 -0500

    key

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dv@c@!(l9khajd%0rrj-hs#=qfz8xiuk+z*$0qc4&61mh%__o='
+SECRET_KEY = os.environ.get('K')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
